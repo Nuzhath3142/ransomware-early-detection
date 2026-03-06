56ce224b940ab3b2c16577b3923f043ed3f4b7d1
Faija shaik <faijashaik@gamil.com>
2026-03-06 16:51:40 +0530

    updated signup and APP.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import Login from './pages/Login'; // Import your new Login component
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup'; // 1. Add this import
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} /> {/* 2. Add this route */}
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Routes>
+    </Router>
   );
 }
 
